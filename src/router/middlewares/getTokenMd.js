5cc880ae8fe1fd7f4f83e7f8bb30aceddbe2a917
EomYoosang <dbtkd1801@gmail.com>
2021-11-10 02:44:26 +0900

    [FEAT] 비밀번호 변경 기능 추가

---
+++ src/router/middlewares/getTokenMd.js
@@ -0,0 +1,26 @@
+import Boom from "@hapi/boom";
+import jwt from "jsonwebtoken";
+
+export const decodeToken = (token) => {
+  const decodedToken = new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) reject(error);
+      resolve(decoded);
+    });
+  });
+  return decodedToken;
+};
+
+const getTokenMd = async (ctx, next) => {
+  const accessToken = ctx.headers.authorization.split(" ")[1];
+  if (!accessToken) Boom.badRequest("invalid token");
+  const decoded = await decodeToken(accessToken);
+  if (Date.now() / 1000 - decoded.iat > 60 * 10) {
+    throw Boom.badRequest("timeout");
+  }
+  ctx.state.reqBody = decoded;
+
+  await next();
+};
+
+export default getTokenMd;
