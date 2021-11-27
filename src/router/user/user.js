789ecb49e4b83736226fb24f1afd4ec4a9907e39
EomYoosang <dbtkd1801@gmail.com>
2021-11-27 17:39:38 +0900

    [BUG] 로그인 안되는 문제

--- src/router/user/user.js
+++ src/router/user/user.js
@@ -58,8 +58,9 @@ export const saveUserMd = async (ctx, next) => {
 export const comparePasswordMd = async (ctx, next) => {
   const { id, password } = ctx.state.reqBody;
   const user = await User.findOne({ id }).exec();
+  console.log(user);
+
   if (user === undefined) {
-    console.log(user);
     throw Boom.badRequest("invalid id");
   }
   const hashed = await crypto
