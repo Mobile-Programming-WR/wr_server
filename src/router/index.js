4d5da0508b56fd03db9b515b2dfde2f28c0db9aa
EomYoosang <dbtkd1801@gmail.com>
2021-11-28 15:33:47 +0900

    FE기 정보 가져오기

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import user from "./user";
 import friend from "./friend";
 import record from "./record";
 import challenge from "./challenge";
+import competition from "./competition";
 
 const router = new Router({
   prefix: "/api/wr/v1",
@@ -12,4 +13,5 @@ router.use("/user", user.routes());
 router.use("/friend", friend.routes());
 router.use("/record", record.routes());
 router.use("/challenge", challenge.routes());
+router.use("/competition", competition.routes());
 export default router;
